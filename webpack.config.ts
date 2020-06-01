73d9ec30289fabcf52b45cdc4d4f968e5bb37392
Taisei Nakajima <ne300085@senshu-u.jp>
2020-06-02 01:18:03 +0900

    first commit

---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import { ConfigurationFactory } from 'webpack'
+import CopyPlugin from 'copy-webpack-plugin'
+
+const config: ConfigurationFactory = () => {
+    return {
+        entry: {
+            popup: `${__dirname}/src/popup.ts`,
+            content: `${__dirname}/src/content.ts`,
+            background: `${__dirname}/src/background.ts`
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".ts", ".js"]
+        },
+        plugins: [
+            new CopyPlugin({
+                patterns: [
+                    {from: 'public', to: '.'}
+                ]
+            }),
+        ]
+    }
+}
+
+export default config
