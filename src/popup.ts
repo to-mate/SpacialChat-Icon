8050708f7ea29aedd792f8a90cb4d3de9359088e
Taisei Nakajima <ne300085@senshu-u.jp>
2020-06-04 17:35:06 +0900

    change image name

--- src/popup.ts
+++ src/popup.ts
@@ -1,5 +1,33 @@
 import Croppie from "croppie"
 import "./popup.css"
+import firebase from "firebase/app"
+import "firebase/storage"
+import "firebase/auth"
+import url from "url"
+
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+}
+firebase.initializeApp(firebaseConfig)
+
+firebase.auth().signInAnonymously().catch((err) => {
+  console.error(err.message)
+})
+
+chrome.tabs.getSelected(tab => {
+  if (tab.url) {
+    const roomName = url.parse(tab.url, true, true).pathname
+    if (roomName) {
+      chrome.storage.local.set({roomName: roomName.replace(/^\/s\//, "")})
+    }
+  }
+})
 
 let uploadCrop: Croppie
 const imageArea = document.getElementById('croppie')
@@ -23,7 +51,7 @@ if (imageArea) {
 const uploadButton = document.getElementById('upload')
 
 uploadButton?.addEventListener('change', function () {
-  readFile(<HTMLInputElement>this)
+  readFile(this as HTMLInputElement)
 })
 
 
@@ -33,7 +61,15 @@ setIconButton?.addEventListener('click', () => {
   uploadCrop.result({
     type: 'blob',
     size: 'viewport'
-  }).then()
+  }).then((resp) => {
+  chrome.tabs.executeScript({ file: 'setMyId.js' }, () => {
+    chrome.storage.local.get(['initial', 'color' ,'roomName'], (result) => {
+      const storageRef = firebase.storage().ref().child(`${result.roomName}/${result.initial + result.color}.png`)
+      storageRef.put(resp).then().catch((err) => { console.error(err.message) })
+    })
+  })
+    setIconLocalStorage(resp)
+  })
 })
 
 
@@ -48,5 +84,14 @@ const readFile = (input: HTMLInputElement) => {
   reader.readAsDataURL((<any>input.files)[0])
 }
 
-
-
+const setIconLocalStorage = (icon: Blob) => {
+  const reader = new FileReader();
+  reader.onload = (e: Event) => {
+    chrome.storage.local.set({ icon: (e as any).target.result }, () => {
+      chrome.tabs.executeScript({
+        file: 'changeIcon.js'
+      })
+    })
+  }
+  reader.readAsDataURL(icon)
+}
